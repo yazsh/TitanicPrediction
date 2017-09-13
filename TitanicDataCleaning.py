f611ccb604f2508ab4071a5e37fa6fd1af11927b
Yazen2017 <yazen.shunnar@gmail.com>
2017-09-13 19:58:51 -0400

    60% acc, so barely working

--- TitanicDataCleaning.py
+++ TitanicDataCleaning.py
@@ -10,6 +10,7 @@ objects = newFeature.select_dtypes(include=["object"])
 numeric = newFeature.select_dtypes(exclude=["object"])
 columns = numeric.columns
 numeric = numeric.fillna(0)
+
 scalar = preprocessing.MinMaxScaler()
 numeric = scalar.fit_transform(numeric)
 numeric = pd.DataFrame(numeric, columns= columns)
@@ -21,12 +22,22 @@ test_feature_objects = objects[separator:]
 
 train_feature_numeric = numeric[:separator]
 test_feature_numeric = numeric[separator:]
-
+print(train_feature_objects.shape)
+print(train_feature_numeric.shape)
 train_feature = pd.concat([train_feature_objects, train_feature_numeric], axis=1)
 test_feature = pd.concat([test_feature_objects, test_feature_numeric], axis=1)
+print(train_feature.shape)
+print(train_labels.shape)
 
-dev_feature = train_feature[700:]
+dev_feature = train_feature[700:-1]
 train_feature = train_feature[:700]
 
 dev_labels = train_labels[700:]
 train_labels = train_labels[:700]
+print(dev_labels.shape)
+print(dev_feature.shape)
+
+train_feature = np.nan_to_num(train_feature)
+dev_feature = np.nan_to_num(dev_feature)
+test_feature = np.nan_to_num(test_feature)
+assert not np.any(np.isnan(train_feature))
--- TitanicPredictor.py
+++ TitanicPredictor.py
@@ -3,17 +3,19 @@ import numpy as np
 import tensorflow as tf
 from HelperFunctions import *
 from TitanicDataCleaning import *
+
+
 input_features = 2433
-hidden1 = 10
-hidden2 = 10
-hidden3 = 10
+hidden1 = 6
+hidden2 = 5
+hidden3 = 2
 hidden4 = 1
 
-learning_rate=.001
+learning_rate=.01
 
 weights = dict(
-            w1=tf.Variable(tf.random_normal([input_features, hidden1]),"adfasf"),
-            w2=tf.Variable(tf.random_normal([hidden1, hidden2]), "222222"),
+            w1=tf.Variable(tf.random_normal([input_features, hidden1]),),
+            w2=tf.Variable(tf.random_normal([hidden1, hidden2]),),
             w3=tf.Variable(tf.random_normal([hidden2, hidden3])),
             w4=tf.Variable(tf.random_normal([hidden3, hidden4]))
             )
@@ -26,23 +28,29 @@ biases = dict(
             )
 
 x = tf.placeholder("float32", [None,input_features])
-layer = create_layer(x, weights['w1'], biases['b1'])
-layer = create_layer(layer, weights['w2'], biases['b2'])
-layer = create_layer(layer, weights['w3'], biases['b3'])
-Z4 = create_layer(layer, weights['w4'], biases['b4'], None)
+
+layer = create_layer(x, weights['w1'], biases['b1'], tf.tanh)
+layer = create_layer(layer, weights['w2'], biases['b2'], tf.tanh)
+layer = create_layer(layer, weights['w3'], biases['b3'], tf.tanh)
+Z4 = create_layer(layer, weights['w4'], biases['b4'])
 
 y = tf.placeholder(dtype="float32",shape=[None,1])
 
-# cost = tf.reduce_mean(tf.cast(tf.equal(Z4, y), "float32"))
+# cost = tf.losses.sigmoid_cross_entropy()
 cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Z4, labels=y))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    print(sess.run(Z4, feed_dict={x: train_feature, y: train_labels})[:10])
-    for i in range(0, 10):
+    for i in range(0, 100):
 
         _, c = sess.run([optimizer, cost], feed_dict={x: train_feature, y: train_labels})
-        # print(sess.run(Z4, feed_dict={x: train_feature, y: train_labels})[:10])
         print("Iteration " + str(i) + ":  " + str(c))
+
+    prediction = tf.round(tf.sigmoid(Z4))
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction,train_labels), "float32"))
+    accuracy2 = tf.reduce_mean(tf.cast(tf.equal(prediction,dev_labels), "float32"))
+
+    print(sess.run(accuracy, feed_dict={x:train_feature, y:train_labels}))
+    print(sess.run(accuracy2, feed_dict={x:dev_feature, y:dev_labels}))
 
